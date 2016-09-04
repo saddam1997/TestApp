435acde1ab319708d59674d7cf64ef39830819a2
saddam.husen1997@gmail.com <saddam.husen1997@gmail.com>
2016-09-04 20:40:09 +0530

    Make Table in View

--- api/controllers/CustomerController.js
+++ api/controllers/CustomerController.js
@@ -24,7 +24,14 @@ module.exports = {
         customer: customer
       });
       });
-
+  },
+  index: function (req,res,next) {
+    Customer.find(function foundCustomers(err,customers){
+      if(err) return next(err);
+      res.view({
+        customers:customers
+      });
+    })
   }
 };
 
